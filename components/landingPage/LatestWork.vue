fa9ec29f9a4ee8bd5fde9cbd3c8509f15e502dfc
Dominik <dom@utom.de>
2023-02-26 21:33:29 +0100

    layout changes

--- components/landingPage/LatestWork.vue
+++ components/landingPage/LatestWork.vue
@@ -83,13 +83,13 @@ onMounted(() => {
           ? (tl.duration() * (container.offsetWidth / 2)) / totalWidth
           : 0;
         center &&
-          times.forEach((t, i) => {
-            times[i] = timeWrap(
-              tl.labels["label" + i] +
-              (tl.duration() * widths[i]) / 2 / totalWidth -
-              timeOffset
-            );
-          });
+        times.forEach((t, i) => {
+          times[i] = timeWrap(
+            tl.labels["label" + i] +
+            (tl.duration() * widths[i]) / 2 / totalWidth -
+            timeOffset
+          );
+        });
       },
       getClosest = (values, value, wrap) => {
         let i = values.length,
@@ -118,13 +118,13 @@ onMounted(() => {
           distanceToLoop =
             distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
           tl.to(
-            item,
-            {
-              xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
-              duration: distanceToLoop / pixelsPerSecond,
-            },
-            0
-          )
+              item,
+              {
+                xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
+                duration: distanceToLoop / pixelsPerSecond,
+              },
+              0
+            )
             .fromTo(
               item,
               {
@@ -164,7 +164,7 @@ onMounted(() => {
     function toIndex(index, vars) {
       vars = vars || {};
       Math.abs(index - curIndex) > length / 2 &&
-        (index += index > curIndex ? -length : length); // always go in the shortest direction
+      (index += index > curIndex ? -length : length); // always go in the shortest direction
       let newIndex = gsap.utils.wrap(0, length, index),
         time = times[newIndex];
       if (time > tl.time() !== index > curIndex) {
@@ -207,9 +207,9 @@ onMounted(() => {
           ),
         syncIndex = () => tl.closestIndex(true);
       typeof InertiaPlugin === "undefined" &&
-        console.warn(
-          "InertiaPlugin required for momentum-based scrolling and snapping. https://greensock.com/club"
-        );
+      console.warn(
+        "InertiaPlugin required for momentum-based scrolling and snapping. https://greensock.com/club"
+      );
       draggable = Draggable.create(proxy, {
         trigger: items[0].parentNode,
         type: "x",
@@ -229,7 +229,7 @@ onMounted(() => {
             snapTime = times[getClosest(times, wrappedTime, tl.duration())],
             dif = snapTime - wrappedTime;
           Math.abs(dif) > tl.duration() / 2 &&
-            (dif += dif < 0 ? tl.duration() : -tl.duration());
+          (dif += dif < 0 ? tl.duration() : -tl.duration());
           return (time + dif) / tl.duration() / -ratio;
         },
         onRelease: syncIndex,
@@ -243,7 +243,7 @@ onMounted(() => {
   }
 
   ctx.value = gsap.context((self) => {
-    const colors = ["#f38630", "#6fb936", "#ccc", "#6fb936"];
+    const colors = ["#f38630","#6fb936", "#ccc", "#6fb936"];
 
     let activeElement;
     const boxes = self.selector(".card");
@@ -251,13 +251,13 @@ onMounted(() => {
     //     backgroundColor: gsap.utils.wrap(colors)
     // });
 
-    gsap.set(".box", { opacity: 0, y: 20 });
+    gsap.set(".box", {opacity: 0, y: 20});
 
     ScrollTrigger.batch(".box", {
-      onEnter: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 }),
-      onLeave: batch => gsap.to(batch, { opacity: 0, y: 20 }),
-      onEnterBack: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 }),
-      onLeaveBack: batch => gsap.to(batch, { opacity: 0, y: 20 }),
+      onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15}),
+      onLeave: batch => gsap.to(batch, {opacity: 0, y: 20}),
+      onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15}),
+      onLeaveBack: batch => gsap.to(batch, {opacity: 0, y: 20}),
 
       start: "top 60%",
       end: "bottom 5%",
@@ -275,7 +275,7 @@ onMounted(() => {
         // activeElement = element;
       },
     });
-    boxes.forEach((box, i) => box.addEventListener("click", () => loop.toIndex(i, { duration: 0.8, ease: "power1.inOut" })));
+    boxes.forEach((box, i) => box.addEventListener("click", () => loop.toIndex(i, {duration: 0.8, ease: "power1.inOut"})));
   }, main.value);
 
 });
@@ -287,43 +287,48 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="bg-white overflow-x-hidden">
+  <div class="bg-white overflow-hidden">
     <div class="mt-44 mb-32 space-y-24" ref="main">
       <!-- heading -->
       <div class="flex flex-col justify-between space-y-10 md:flex-row md:space-y-0">
         <div class="flex items-center">
-          <h1 class="text-[3.75rem] font-extrabold leading-tight text-black lg:text-[5rem] box">
+          <h1 class="text-[3.75rem] font-InterExtraBold  leading-tight text-black lg:text-[5rem] box">
             Latest Works
           </h1>
-          <a href="/" class="group mb-3 ml-20 hidden self-end text-lg font-semibold text-black box
-                           hover:text-[#ff4437] hover:transition-all hover:duration-[0.6s] lg:flex">ALL
+          <a href="/"
+             class="group mb-3 ml-20 hidden self-end text-lg font-semibold text-black box
+                         hover:text-[#ff4437] hover:transition-all hover:duration-[0.6s] lg:flex">ALL
             PROJECTS
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="h-7 w-7 group-hover:translate-x-1 group-hover:stroke-[#ff4437] 
-                                  group-hover:transition-all group-hover:duration-[0.6s]" viewBox="0 0 24 24"
-              strokeWidth="{1.5}" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                 class="h-7 w-7 group-hover:translate-x-1 group-hover:stroke-[#ff4437]
+                                group-hover:transition-all group-hover:duration-[0.6s]"
+                 viewBox="0 0 24 24" strokeWidth="{1.5}" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
             </svg>
           </a>
         </div>
         <div class="flex flex-row space-x-5 pb-3 md:flex-col md:space-x-0 lg:flex-row lg:space-x-5">
           <div
-            class="box group flex h-12 w-12 items-center justify-center self-end border-[1.5px] 
-                          border-black/30 hover:relative hover:cursor-pointer hover:overflow-hidden hover:border-0 hover:bg-[#ff4437] hover:duration-[0.3s]">
+            class="box group flex h-12 w-12 items-center justify-center self-end border-[1.5px]
+                        border-black/30 hover:relative hover:cursor-pointer hover:overflow-hidden hover:border-0 hover:bg-[#ff4437] hover:duration-[0.3s]">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="h-6 w-6 stroke-gray-400 group-hover:absolute group-hover:right-[-20%] 
-                              group-hover:-translate-x-5 group-hover:stroke-white group-hover:transition-all 
-                              group-hover:duration-[0.4s]">
+                 stroke="currentColor"
+                 class="h-6 w-6 stroke-gray-400 group-hover:absolute group-hover:right-[-20%]
+                            group-hover:-translate-x-5 group-hover:stroke-white group-hover:transition-all
+                            group-hover:duration-[0.4s]">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
             </svg>
           </div>
 
-          <div class="box group flex h-12 w-12 items-center justify-center self-end border-[1.5px] 
-                          border-black/30 hover:relative hover:cursor-pointer hover:border-0 hover:bg-[#ff4437] 
-                          hover:duration-[0.3s]">
+          <div
+            class="box group flex h-12 w-12 items-center justify-center self-end border-[1.5px]
+                        border-black/30 hover:relative hover:cursor-pointer hover:border-0 hover:bg-[#ff4437]
+                        hover:duration-[0.3s]">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="left-[-20%] h-6 w-6 stroke-gray-400 group-hover:absolute group-hover:translate-x-5
-                               group-hover:stroke-white group-hover:transition-all group-hover:duration-[0.4s]">
+                 stroke="currentColor"
+                 class="left-[-20%] h-6 w-6 stroke-gray-400 group-hover:absolute group-hover:translate-x-5
+                             group-hover:stroke-white group-hover:transition-all group-hover:duration-[0.4s]">
               <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
             </svg>
           </div>
@@ -331,12 +336,13 @@ onUnmounted(() => {
       </div>
 
       <!-- slider -->
-      <div class="slider-wrapper flex flex-rows space-x-10 ">
+      <div class="slider-wrapper flex flex-rows space-x-10 " >
         <div class="card group space-y-10 mx-10 ">
           <div class="box overflow-hidden">
             <img src="https://dev.utom.berlin/assets/img/portfolio/work-1.png"
-              class="image w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0 
-                              group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110" alt="" />
+                 class="image w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0
+                            group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110"
+                 alt="" />
           </div>
           <div>
             <div
@@ -352,16 +358,19 @@ onUnmounted(() => {
         <div class="card group space-y-10 mx-10">
           <div class="box overflow-hidden">
             <img src="https://dev.utom.berlin/assets/img/portfolio/work-2.png"
-              class="w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0 
-                              group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110" alt="" />
+                 class="w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0
+                            group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110"
+                 alt="" />
           </div>
           <div>
-            <div class="mb-3 text-base font-medium uppercase text-[#666666] group-hover:text-black/75 
-                              group-hover:transition-all group-hover:duration-[1s]">
+            <div
+              class="mb-3 text-base font-medium uppercase text-[#666666] group-hover:text-black/75
+                            group-hover:transition-all group-hover:duration-[1s]">
               product
             </div>
-            <h4 class="text-3xl font-bold text-[#666666] hover:cursor-pointer hover:underline hover:duration-[1s] 
-                              group-hover:text-black group-hover:transition-all group-hover:duration-[1s]">
+            <h4
+              class="text-3xl font-bold text-[#666666] hover:cursor-pointer hover:underline hover:duration-[1s]
+                            group-hover:text-black group-hover:transition-all group-hover:duration-[1s]">
               The Battery Show Europe 2021
             </h4>
           </div>
@@ -369,16 +378,19 @@ onUnmounted(() => {
         <div class="card group space-y-10 mx-10">
           <div class="box overflow-hidden">
             <img src="https://dev.utom.berlin/assets/img/portfolio/work-3.png"
-              class="w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0 
-                              group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110" alt="" />
+                 class="w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0
+                            group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110"
+                 alt="" />
           </div>
           <div>
-            <div class="mb-3 text-base font-medium uppercase text-[#666666] group-hover:text-black/75 
-                              group-hover:transition-all group-hover:duration-[1s]">
+            <div
+              class="mb-3 text-base font-medium uppercase text-[#666666] group-hover:text-black/75
+                            group-hover:transition-all group-hover:duration-[1s]">
               Branding,Packaging
             </div>
-            <h4 class="text-3xl font-bold text-[#666666] hover:cursor-pointer hover:underline hover:duration-[1s] 
-                              group-hover:text-black group-hover:transition-all group-hover:duration-[1s]">
+            <h4
+              class="text-3xl font-bold text-[#666666] hover:cursor-pointer hover:underline hover:duration-[1s]
+                            group-hover:text-black group-hover:transition-all group-hover:duration-[1s]">
               Bemusterungstool
             </h4>
           </div>
@@ -386,16 +398,19 @@ onUnmounted(() => {
         <div class="card group space-y-10 mx-10">
           <div class="box overflow-hidden">
             <img src="https://dev.utom.berlin/assets/img/portfolio/work-3.png"
-              class="w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0 
-                              group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110" alt="" />
+                 class="w-[65rem] object-contain grayscale hover:cursor-pointer group-hover:grayscale-0
+                            group-hover:transition-all group-hover:duration-[1s] transition-all ease-out hover:scale-110"
+                 alt="" />
           </div>
           <div>
-            <div class="mb-3 text-base font-medium uppercase text-[#666666] group-hover:text-black/75 
-                              group-hover:transition-all group-hover:duration-[1s]">
+            <div
+              class="mb-3 text-base font-medium uppercase text-[#666666] group-hover:text-black/75
+                            group-hover:transition-all group-hover:duration-[1s]">
               Branding,Packaging
             </div>
-            <h4 class="text-3xl font-bold text-[#666666] hover:cursor-pointer hover:underline hover:duration-[1s] 
-                              group-hover:text-black group-hover:transition-all group-hover:duration-[1s]">
+            <h4
+              class="text-3xl font-bold text-[#666666] hover:cursor-pointer hover:underline hover:duration-[1s]
+                            group-hover:text-black group-hover:transition-all group-hover:duration-[1s]">
               Bemusterungstool
             </h4>
           </div>
@@ -425,4 +440,6 @@ onUnmounted(() => {
   width: 150vw;
 
 }
+
+
 </style>
